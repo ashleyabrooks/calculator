b9ef6e6bd82e8f953d8e86c36f5dbc712297065d
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-04 15:55:50 -0800

    Added all function calls

--- calculator.py
+++ calculator.py
@@ -2,10 +2,10 @@ from arithmetic import *
 
 while True:
     user_input = raw_input("> ")
-    tokens = user_input.split(" ")
+    tokens = user_input.split()
 
     if tokens[0] == 'q':
-        exit()
+        break
 
     if tokens[0] == '+':
         print add(int(tokens[1]), int(tokens[2]))
@@ -19,3 +19,9 @@ while True:
         print square(int(tokens[1]))
     elif tokens[0] == 'cube':
         print cube(int(tokens[1]))
+    elif tokens[0] == 'pow':
+        print power(int(tokens[1]), int(tokens[2]))
+    elif tokens[0] == 'mod':
+        print mod(int(tokens[1]), int(tokens[2]))
+    else:
+        print "I don't understand that input"
